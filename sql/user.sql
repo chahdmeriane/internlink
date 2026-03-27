2cbefe40f4ed1a136cd86ed76740c6cae501d765
chahdmeriane <chahd.meriane2004@gmail.com>
2026-03-27 20:16:11 +0100

    updated

--- sql/user.sql
+++ sql/user.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('student','company') NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    id         INT UNSIGNED   NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100)   NOT NULL,
+    last_name  VARCHAR(100)   NOT NULL,
+    email      VARCHAR(150)   NOT NULL UNIQUE,
+    password   VARCHAR(255)   NOT NULL,
+    role       ENUM('student','company','admin') NOT NULL,
+    created_at DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
