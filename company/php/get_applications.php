67fac6f3fa7ad6272aacc8a03bd0da62af2c0c33
chahdmeriane <chahd.meriane2004@gmail.com>
2026-03-30 00:50:47 +0200

    Update get_applications.php

--- company/php/get_applications.php
+++ company/php/get_applications.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header('Content-Type: application/json');
 require_once __DIR__ . '/db.php';
 require_once __DIR__ . '/auth_guard.php';
