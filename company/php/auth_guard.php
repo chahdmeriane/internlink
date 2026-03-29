ce4528a6580beb8873e35ee632200415f0936d47
chahdmeriane <chahd.meriane2004@gmail.com>
2026-03-30 00:48:01 +0200

    Update auth_guard.php

--- company/php/auth_guard.php
+++ company/php/auth_guard.php
@@ -1,11 +1,12 @@
 <?php
 error_reporting(0);
 ini_set('display_errors', 0);
+ini_set('session.cookie_path', '/');
 
-// Only start session if not already started
+session_save_path(sys_get_temp_dir());
+session_name('internlink_session');
+
 if (session_status() === PHP_SESSION_NONE) {
-    session_save_path(sys_get_temp_dir());
-    session_name('internlink_session');
     session_start();
 }
 
